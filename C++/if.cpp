06796f6aa0f962438a2123666c05ce40f4d236a4
mahesh-web3 <mingole555@gmail.com>
2024-08-14 00:20:17 +0530

    Create if.cpp

---
+++ C++/if.cpp
@@ -0,0 +1,27 @@
+
+                  
+    # include<iostream>
+    int main()
+    {
+        int a, b, c ;
+       
+        printf("Enter 3 no.s");
+        
+        scanf("%d,%d,%d",&a,&b,&c);
+       
+        if(a>=b&&a>=c)
+         
+           printf("%d is greater ",a);
+            
+            else
+             
+             if(b>=c&&b>=a)
+            
+              printf("% is greater",b);
+            
+               else
+               
+               printf("%d is greater",c);
+              
+              return 0; 
+    }
